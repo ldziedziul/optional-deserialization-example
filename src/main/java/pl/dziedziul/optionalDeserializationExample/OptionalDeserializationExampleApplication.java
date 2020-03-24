cfe04b99f839719cd7336aefdcb38d1b36033971
Łukasz Dziedziul <l.dziedziul@gmail.com>
2020-03-24 11:40:01 +0100

    init commit

---
+++ src/main/java/pl/dziedziul/optionalDeserializationExample/OptionalDeserializationExampleApplication.java
@@ -0,0 +1,13 @@
+package pl.dziedziul.optionalDeserializationExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OptionalDeserializationExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OptionalDeserializationExampleApplication.class, args);
+    }
+
+}
